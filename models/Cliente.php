19e3717117dd7aab0dd7bb946e63eda27c6ff4fd
alcPresunto <guilhermeasouza906@gmail.com>
2025-12-06 12:21:14 -0300

    Edição na Classe Cliente

--- models/Cliente.php
+++ models/Cliente.php
@@ -2,34 +2,5 @@
 
 class Cliente extends Usuario
 {
-    private string $endereco;
-    private string $telefone;
 
-    /**
-     * Getters
-     */
-
-    public function getEndereco(): string
-    {
-        return $this->endereco;
-    }
-
-    public function getTelefone(): string
-    {
-        return $this->telefone;
-    }
-
-    /**
-     * Setters
-     */
-
-    public function setEndereco(string $endereco): void
-    {
-        $this->endereco = $endereco;
-    }
-
-    public function setTelefone(string $telefone): void
-    {
-        $this->telefone = $telefone;
-    }
 }
